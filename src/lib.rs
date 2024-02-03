9a2dfdba862780f68c6314d958126186ab28e13d
yashshah7197 <yash@yashshah.io>
2024-02-03 14:40:20 +0530

    Add health check endpoint

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use std::net::TcpListener;
+use actix_web::{web, App, HttpServer, HttpResponse};
+use actix_web::dev::Server;
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+        .listen(listener)?
+        .run();
+
+    Ok(server)
+}
